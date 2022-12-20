a686a69acc838f89e991bd6114c97297b68d9824
NQuiros12 <nicolasquiros12@gmail.com>
2022-12-20 00:49:23 -0300

    For some reason the line 24 to 25 is not working as expected, more insights tomorrow

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
-from flask import Flask
+from flask import Flask, make_response,jsonify
+import json
+from db import Connector
 app = Flask(__name__)
 
-in_memory_datastore = {
+""" in_memory_datastore = {
    "COBOL" : {"name": "COBOL", "publication_year": 1960, "contribution": "record data"},
    "ALGOL" : {"name": "ALGOL", "publication_year": 1958, "contribution": "scoping and nested functions"},
    "APL" : {"name": "APL", "publication_year": 1962, "contribution": "array processing"},
 }
-
-
-@app.get('/prog_lg')
-def list_programming_languages():
-   return {"programming_languages":list(in_memory_datastore.values())}
-
-@app.route('/prog_lg/<programming_language_name>')
-def get_programming_language(programming_language_name):
-   return in_memory_datastore[programming_language_name]
-@app.get("/prog_lg/test")
+ """
+conn = Connector("paises","localhost","root","micolash12")
+conn.connect()
+@app.get('/countries')
+def countries():
+   conn.execute("select * from pais limit 1;") ##### Modificar esta query
+   rs = conn.fetchall()
+   results_json = json.dumps([{"code": r[0], "capital": r[1]} for r in rs])
+   return results_json  
+@app.route('/countries/<countrie>')
+def countrie(countrie):
+   conn.execute(f"select * from pais where nombre = '{countrie}' limit 1;") ##### Modificar esta query
+   rs = conn.fetchone()
+   #For some reason this is getting me a float.
+   #results_json = json.dumps([{"code": r[0], "name": r[1]} for r in rs])
+   return jsonify(rs)
+@app.get("/countries/test")
 def test_hi():
-   return {"I'm working"}
+   response = make_response('the API works!', 200)
+   response.mimetype = 'text/plain'
+   return response
--- db.py
+++ db.py
@@ -1,22 +1,40 @@
-import sqlite3
+import mysql.connector
 
-import click
-from flask import current_app, g
-
-
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
+class Connector(object):
+  #Create a constructor
+  def __init__(self,database:str,host:str,user:str,password:str):
+    self.database = database
+    self.host = host
+    self.user = user
+    self.password = password
+  #Create a method to connect to the database
+  def connect(self):
+    self.conn = mysql.connector.connect(
+      host = self.host,
+      user = self.user,
+      password = self.password,
+      database = self.database
+    )
+    self.cursor = self.conn.cursor()
+    if(self.cursor):
+      print("Connected successfully!")
+  #Execute a query in the database
+  def execute(self, query):
+    self.cursor.execute(query)
+  #Fetch the result but only return the first result
+  def fetchone(self):
+    return self.cursor.fetchone()
+  #Fetch the result and return all the set.
+  def fetchall(self):
+    return self.cursor.fetchall()
+  #Close the db connection.
+  def close(self):
+    self.cursor.close()
+    if(False == self.cursor):
+      print("Closed successfully!")
+  
+""" con = Connector("paises","localhost","root","micolash12")
+con.connect()
+con.execute("select * from pais limit 4")
+print(con.fetchall())
+print(con.close()) """
