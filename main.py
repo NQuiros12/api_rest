a5108e816ca9b5ebc57fa82afb91553760742852
NQuiros12 <nicolasquiros12@gmail.com>
2023-01-04 17:40:05 -0300

    Now I create several more features, we only be missing the DELETE and PUT method for both books and authors. And also review the books genre feature

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from config.db import Connector
 from pydantic import BaseModel
-
+from starlette.responses import FileResponse 
 class Book(BaseModel):
-    id : int
+    id : int | None = None
     title: str
     author_id: int
     genre : str
@@ -22,24 +22,38 @@ app = FastAPI()
 # #Create the connection to the database
 conn = Connector("bookstore","localhost","root","micolash12",3306)
 conn.connect()
+#This function tell the number of rows in the table, this is much needed in the different endpoints of this API
 def rows_count(table : str) -> int:
     conn.execute(f"select count(*) from {table};")
     return conn.fetchone()[0]
+#This gets the first 10 books.
 @app.get('/books')
 async def books():
    conn.execute("select * from book limit 10;") ##### Modificar esta query
    res = conn.fetchall()
    return res 
+@app.get('/books_genre/{genre}')
+async def books_genre(genre : str):
+    genre = genre.lower()
+    print(genre)
+    conn.execute("select * from book where genre = '{genre}' limit 5;") 
+    res = conn.fetchall()
+    return res 
+
 @app.get('/authors')
 async def authors():
    conn.execute("select * from author limit 10;") ##### Modificar esta query
    res = conn.fetchall()
+   return res
+@app.get('/author/{name}')
+async def author(name:str):
+   conn.execute(f"select * from author where name = '{name}';") ##### Modificar esta query
+   res = conn.fetchall()
    return res 
-@app.get('/author/{lastname}')
-async def book_by_author_lastname( lastname : str ):
-    conn.execute(f"select id_author from author where last_name = '{lastname}';")
+@app.get('/author_books/{last_name}')
+async def books_by_author_lastname( last_name : str ):
+    conn.execute(f"select id_author from author where last_name = '{last_name}';")
     author_id = conn.fetchone()
-    #print(author_id)
     conn.execute(f"select * from book where id_author = {author_id[0]} ;") ##### Modificar esta query
     rs = conn.fetchall()
     return rs
@@ -52,12 +66,15 @@ async def add_author(author:Author):
     return {"message": "Author created successfully"}
 @app.post('/book/')
 async def add_book(book : Book):
-    conn.execute(f"""insert into book(index,title,genre,height,publisher,id_author) values(%s,%s,%s,%s)""")
+    conn.execute(f"""insert into book(title,genre,height,publisher,id_author) values('{book.title}',
+                                                                        '{book.genre.capitalize()}',
+                                                                        {book.height},
+                                                                        '{book.publisher}',
+                                                                        {book.author_id})""")
     conn.conn.commit()
     conn.close()
     return {"message": "Book created successfully"}
 
 @app.get("/",response_class=HTMLResponse)
 def test_hi():
-   response = '<html><h1>the API works!</h1></html>'
-   return response
+   return FileResponse("template/index.html")
